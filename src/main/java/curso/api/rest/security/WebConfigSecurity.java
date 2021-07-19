3923f21c680acac123a92e89b85cd2534cc3cb2c
francisco.igor <figortabosa@gmail.com>
2021-07-19 15:50:12 -0300

    finalizando configuração JWT

--- src/main/java/curso/api/rest/security/WebConfigSecurity.java
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
@@ -36,13 +37,15 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter{
 			/*URL de logout - Redireciona após o user deslogar do sistema*/
 			.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 			
-			/**Mapeia URL  de logout e invalida o usuario*/
+			/*Mapeia URL  de logout e invalida o usuario*/
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			;
 			
+			/*Filtra requisições de login para autenticação*/
+			.and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+											UsernamePasswordAuthenticationFilter.class)
 			
-			
-			
+			/*Filtra demais requisições para verificar a presença do TOKEM JWT no Header HTTP*/
+			.addFilterBefore(new JWTApiAtenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
 		}
 	
 	
